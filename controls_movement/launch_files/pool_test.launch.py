edee0bbc26cc83ac6d4abcd7e21381583a7f0bc6
Nathan Ubuntu VM 3 <nathantew14@gmail.com>
2025-01-08 12:07:19 +0800

    make launch file work

--- controls_movement/launch_files/pool_test.launch.py
+++ controls_movement/launch_files/pool_test.launch.py
@@ -16,8 +16,13 @@ from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 # This function is always needed
 def generate_launch_description():
-    main_run = Node(package="controls_movement", executable="moveLeft") 
+    # main_run = Node(package="controls_movement", executable="moveLeft") 
     # change this line to move different direction
     #TODO make it a parameter
-    ld = [main_run]
+    ld = [
+        Node(package="quali_gate_detector", executable="detector"),
+        Node(package="quali_gate_detector", executable="detector_listener"),
+        Node(package="controls_movement", executable="qualiGate"),
+        Node(package="controls_movement", executable="qualiGate_pub"),
+    ]
     return LaunchDescription(ld)

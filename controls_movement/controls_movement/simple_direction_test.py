84a488ef7fdb075deb666ce4612e7180f4f7ea5e
Izen9835 <e1406600@u.nus.edu>
2025-01-15 02:40:20 +0800

    added simple direction controls test script

---
+++ controls_movement/controls_movement/simple_direction_test.py
@@ -0,0 +1,39 @@
+import rclpy
+from rclpy.node import Node
+
+from controls_movement.thruster_allocator import ThrustAllocator
+from thrusters.thrusters import ThrusterControl   
+
+from control_panel.control_panel import create_control_panel, ControlPanelItem as CPI #this is a package in PL repo
+
+
+
+'''
+[X, Y, Z]
+- +X is right
+- +Y is forward
+- +Z is up
+
+[R, P, Y]
+ - +R is roll right
+ - +P is pitch up
+ - +Y is yaw left
+'''
+
+def main():
+    thrusterAllocator = ThrustAllocator
+    #thrusterController = ThrusterControl
+
+    while True:
+
+        Translation = [0, 0, 0]
+
+        #Rotation = [0, 0, 0]
+        #if all(val == 0 for val in Translation):
+
+        thruster_pwm = thrusterAllocator.getTranslationPwm(Translation)
+
+        self.thrusterControl.setThrusters(thrustValues=thruster_pwm)
+
+if __name__ == "__main__":
+    main()

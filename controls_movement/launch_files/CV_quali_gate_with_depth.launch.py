de9017b82699bbfc334cec21009e44a53c348c31
hxbb2024 <hxbb2024@gmail.com>
2025-02-10 19:39:25 +0800

    added a file for CV quali gate detection with depth PID

---
+++ controls_movement/launch_files/CV_quali_gate_with_depth.launch.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import os
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import (
+    ExecuteProcess,
+    IncludeLaunchDescription,
+    include_launch_description,
+)
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+# This function is always needed
+def generate_launch_description():
+    #direction_test_config_path = PathJoinSubstitution(
+    #    [FindPackageShare('controls_movement'), 'config', 'simple_direction_test.yaml']
+    #)
+    quali_gate_pid_config_path = PathJoinSubstitution(
+        [FindPackageShare('controls_movement'), 'config', 'quali_gate_pid.yaml']
+    )
+    vert_pid_config_path = PathJoinSubstitution(
+        [FindPackageShare('controls_movement'), 'config', 'vert_pid_test.yaml']
+    )
+    thruster_config_path = PathJoinSubstitution(
+        [FindPackageShare('controls_movement'), 'config', 'thruster.yaml']
+    )
+    quali_gate_detector_config_path = PathJoinSubstitution(
+        [FindPackageShare('quali_gate_detector'), 'config', 'quali_gate_detector.yaml']
+    )
+    # main_run = Node(package="controls_movement", executable="moveLeft") 
+    # change this line to move different direction
+    #TODO make it a parameter
+    ld = [
+        Node(package="can_handler", executable="can_handler"),
+        Node(package="controls_movement", executable="movementControls", parameters=[thruster_config_path]),
+        Node(package="controls_movement", executable="vertPID", parameters=[vert_pid_config_path]),
+        #Node(package="controls_movement", executable="integratedDirTest", parameters=[direction_test_config_path])
+        Node(package="controls_movement", executable="qualiGate", parameters=[quali_gate_pid_config_path]),
+        Node(package="quali_gate_detector", executable="detector", parameters=[quali_gate_detector_config_path])
+    ]
+    return LaunchDescription(ld)
+    

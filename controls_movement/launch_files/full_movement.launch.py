1e8e82e508f0b136613998f08ab9bbabf85e8ccb
hxbb2024 <hxbb2024@gmail.com>
2025-02-10 17:18:35 +0800

    seems to work, but can't rlly get the gate detection to work in the lab

--- controls_movement/launch_files/full_movement.launch.py
+++ controls_movement/launch_files/full_movement.launch.py
@@ -37,7 +37,8 @@ def generate_launch_description():
         Node(package="controls_movement", executable="movementControls", parameters=[thruster_config_path]),
         Node(package="controls_movement", executable="vertPID", parameters=[vert_pid_config_path]),
         #Node(package="controls_movement", executable="integratedDirTest", parameters=[direction_test_config_path])
-        Node(package="controls_movement", executable="qualiGate", parameters=[quali_gate_pid_config_path])
+        Node(package="controls_movement", executable="qualiGate", parameters=[quali_gate_pid_config_path]),
+        Node(package="quali_gate_detector", executable="obj_detector")
     ]
     return LaunchDescription(ld)
     

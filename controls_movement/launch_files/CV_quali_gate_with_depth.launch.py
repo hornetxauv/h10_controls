e448d5ad33e6f8a7b198942ecb13d04736188dd3
hxbb2024 <hxbb2024@gmail.com>
2025-02-17 21:13:37 +0800

    make the CV launch file only launch the detector node for testing

--- controls_movement/launch_files/CV_quali_gate_with_depth.launch.py
+++ controls_movement/launch_files/CV_quali_gate_with_depth.launch.py
@@ -20,27 +20,29 @@ def generate_launch_description():
     #direction_test_config_path = PathJoinSubstitution(
     #    [FindPackageShare('controls_movement'), 'config', 'simple_direction_test.yaml']
     #)
-    quali_gate_pid_config_path = PathJoinSubstitution(
-        [FindPackageShare('controls_movement'), 'config', 'quali_gate_pid.yaml']
-    )
-    vert_pid_config_path = PathJoinSubstitution(
-        [FindPackageShare('controls_movement'), 'config', 'vert_pid_test.yaml']
-    )
-    thruster_config_path = PathJoinSubstitution(
-        [FindPackageShare('controls_movement'), 'config', 'thruster.yaml']
-    )
+    # quali_gate_pid_config_path = PathJoinSubstitution(
+    #     [FindPackageShare('controls_movement'), 'config', 'quali_gate_pid.yaml']
+    # )
+    # vert_pid_config_path = PathJoinSubstitution(
+    #     [FindPackageShare('controls_movement'), 'config', 'vert_pid_test.yaml']
+    # )
+    # thruster_config_path = PathJoinSubstitution(
+    #     [FindPackageShare('controls_movement'), 'config', 'thruster.yaml']
+    # )
     quali_gate_detector_config_path = PathJoinSubstitution(
         [FindPackageShare('quali_gate_detector'), 'config', 'quali_gate_detector.yaml']
     )
+    # quali_gate_detector_config_path = PathJoinSubstitution(
+    #     [FindPackageShare('controls_movement'), 'config', 'quali_gate_detector.yaml']
+    # )
     # main_run = Node(package="controls_movement", executable="moveLeft") 
     # change this line to move different direction
     #TODO make it a parameter
     ld = [
-        Node(package="can_handler", executable="can_handler"),
-        Node(package="controls_movement", executable="movementControls", parameters=[thruster_config_path]),
-        Node(package="controls_movement", executable="vertPID", parameters=[vert_pid_config_path]),
-        #Node(package="controls_movement", executable="integratedDirTest", parameters=[direction_test_config_path])
-        Node(package="controls_movement", executable="qualiGate", parameters=[quali_gate_pid_config_path]),
+        # Node(package="can_handler", executable="can_handler"),
+        # Node(package="controls_movement", executable="movementControls", parameters=[thruster_config_path]),
+        # Node(package="controls_movement", executable="vertPID", parameters=[vert_pid_config_path]),
+        # Node(package="controls_movement", executable="qualiGate", parameters=[quali_gate_pid_config_path]),
         Node(package="quali_gate_detector", executable="detector", parameters=[quali_gate_detector_config_path])
     ]
     return LaunchDescription(ld)

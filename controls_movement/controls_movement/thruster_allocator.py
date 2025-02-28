58e551a82ca63b8b0d35af8c63490d7b37a33ee1
bb <(HX) Samuel FF-DRONE>
2025-02-28 16:56:08 +0700

    remove unnecessary logging

--- controls_movement/controls_movement/thruster_allocator.py
+++ controls_movement/controls_movement/thruster_allocator.py
@@ -143,11 +143,11 @@ class ThrustAllocator(Node):
                     self.get_value('B_MR')/100,    # Middle Right
                     self.get_value('B_MM')/100,]) # Middle Middle
         # self.get_logger().info(f"{thruster_biases[0]}")
-        counter = 0
+        # counter = 0
         for force, bias, back_bias in zip(thrust_forces, thruster_biases, back_thruster_biases):
-            if counter == 0:
-                self.get_logger().info(f"bias {bias} force:{force}")
-            counter += 1
+            # if counter == 0:
+            #     self.get_logger().info(f"bias {bias} force:{force}")
+            # counter += 1
             force = force * (bias if force < 0 else back_bias)
             idx = np.searchsorted(self.thrust_map[:, 0], force, 'left')
             # self.get_logger().info("finish searching")
